3245534c5a9c60519de6b3804408b18a3022accc
Quiara Kirkland <qrkirkland@gmail.com>
2018-06-11 20:13:08 -0400

    Initial Commit

---
+++ Flashlight/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Flashlight
+//
+//  Created by Quiara Kirkland on 6/11/18.
+//  Copyright © 2018 Quiara Kirkland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
